72352c5b5b0329638f3b16899d3b8471ae585642
SupremeKim <45954373+iliasChymas@users.noreply.github.com>
2022-03-04 18:57:57 +0200

    Last changes
    Now the script it takes the updated cache from the guy who started the thread and it merges it with ur current one in use

--- cache_graber.sh
+++ cache_graber.sh
@@ -7,7 +7,7 @@ function download_tool() {
 }
 
 [ ! -f "dxvk-cache-tool" ] && download_tool
-curl -A "Mozila/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s https://old.reddit.com/r/linux_gaming/comments/t5xrho/dxvk_state_cache_for_fixing_stutter_in_apex/ | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep "cdn.dis\|oshi.at" | grep "r5apex.dxvk-cache" | uniq > temp.links
+url=$(curl -A "Mozila/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s https://old.reddit.com/r/linux_gaming/comments/t5xrho/dxvk_state_cache_for_fixing_stutter_in_apex/ | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep "r5apex.dxvk-cache" | head -n 1)
 current_shader="$HOME/.steam/steam/steamapps/shadercache/1172470/DXVK_state_cache/r5apex.dxvk-cache"
 
 if [ "$(ps -e | grep -c steam)" -ne 0 ]
@@ -16,12 +16,7 @@ then
 	sleep 3 #my pc is kind slow
 fi
 
-while read -r line; do
-	wget -q -O output.dxvk-cache $line > /dev/null 2>&1
-	sleep .5
-	./dxvk-cache-tool $current_shader "output.dxvk-cache"
-
-done < temp.links
-
+wget -q -O output.dxvk-cache $url > /dev/null 2>&1
+sleep .5
+./dxvk-cache-tool $current_shader "output.dxvk-cache"
 rm $current_shader && mv output.dxvk-cache $current_shader 
-
