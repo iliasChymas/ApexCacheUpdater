361ff43bcedbda4952e77244e77afe37b65c838e
SupremeKim <45954373+iliasChymas@users.noreply.github.com>
2022-03-04 18:44:41 +0200

    Chamges
    Now it basically takes all download links for apex-cache and merges them all to build a bigger cache

--- cache_graber.sh
+++ cache_graber.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
-[ ! -f ./checked_links ] && touch checked_links
-curl -A "Mozila/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s https://old.reddit.com/r/linux_gaming/comments/t5xrho/dxvk_state_cache_for_fixing_stutter_in_apex/ | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep "cdn.dis\|oshi.at" | grep "r5apex.dxvk-cache" > temp.links
-current_shader=$(wc -c ~/.steam/steam/steamapps/shadercache/1172470/DXVK_state_cache/r5apex.dxvk-cache | cut -d' ' -f 1)
-links_to_check=$(diff checked_links temp.links | grep ">" | sed 's/> //g')
-shaders_location="$HOME/.steam/steam/steamapps/shadercache/1172470/DXVK_state_cache/"
-echo $links_to_check | sed 's/ /\n/g' | uniq > links_to_check
 
-changed=0
+function download_tool() {
+	mkdir -p dxvk_tool
+	wget -P ./dxvk_tool https://lutris.nyc3.cdn.digitaloceanspaces.com/games/overwatch/merge-tool.tar.xz > /dev/null 2>&1
+	tar -xf ./dxvk_tool/merge-tool.tar.xz > /dev/null 2>&1
+}
+
+[ ! -f "dxvk-cache-tool" ] && download_tool
+curl -A "Mozila/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s https://old.reddit.com/r/linux_gaming/comments/t5xrho/dxvk_state_cache_for_fixing_stutter_in_apex/ | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep "cdn.dis\|oshi.at" | grep "r5apex.dxvk-cache" | uniq > temp.links
+current_shader="$HOME/.steam/steam/steamapps/shadercache/1172470/DXVK_state_cache/r5apex.dxvk-cache"
+
+if [ "$(ps -e | grep -c steam)" -ne 0 ]
+then
+	pkill steam
+	sleep 3 #my pc is kind slow
+fi
+
 while read -r line; do
-        echo "$line" >> checked_links
-        wget -q -O r5apex.dxvk-cache $line > /dev/null 2>&1
-        sleep 1
-        if [ "$current_shader" -lt "$(wc -c r5apex.dxvk-cache | cut -d' ' -f 1)" ]
-        then
-                if [ "$(ps -e | grep -c steam)" -ne 0 ]
-                then
-                        pkill steam
-                        sleep 3 #my pc is kind slow
-                fi
+	wget -q -O output.dxvk-cache $line > /dev/null 2>&1
+	sleep .5
+	./dxvk-cache-tool $current_shader "output.dxvk-cache"
 
-                mv "$shaders_location"r5apex.dxvk-cache "$shaders_location"r5apex.dxvk-cache.old"$(ls -l $shaders_location | wc -l)"
-                mv "r5apex.dxvk-cache" "$shaders_location"
-                current_shader=$(wc -c "$HOME"/.steam/steam/steamapps/shadercache/1172470/DXVK_state_cache/r5apex.dxvk-cache | cut -d' ' -f 1)
-                changed=1
-        fi
-done < links_to_check
+done < temp.links
 
-case $changed in
-        1)
-                echo "Found better cache"
-                ;;
-        0)
-                echo "You have da best"
-                ;;
-esac
+rm $current_shader && mv output.dxvk-cache $current_shader 
 
-rm temp.links links_to_check
-[ -f r5apex.dxvk-cache ] && rm r5apex.dxvk-cache
