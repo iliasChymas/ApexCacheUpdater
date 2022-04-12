1080144e3d487950cd8e1ad56ed3e152154b50b1
iliasChymas <iliasx1911@gmail.com>
2022-04-12 16:52:15 +0300

    Bug fixes

--- cache_graber.sh
+++ cache_graber.sh
@@ -8,19 +8,21 @@ function download_tool() {
 
 [ ! -f "dxvk-cache-tool" ] && download_tool
 url=$(curl -A "Mozila/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" -s https://old.reddit.com/r/linux_gaming/comments/t5xrho/dxvk_state_cache_for_fixing_stutter_in_apex/ | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*" | grep "r5apex.dxvk-cache" | head -n 1)
-current_shader="$HOME/.steam/steam/steamapps/shadercache/1172470/DXVK_state_cache/r5apex.dxvk-cache"
-
-if [ "$(ps -e | grep -c steam)" -ne 0 ]
+search_shader="$(find $HOME -name r5apex.dxvk-cache)"
+if [ ! -z $search_shader ]
 then
-	pkill steam
-	sleep 3 #my pc is kind slow
+    if [ "$(ps -e | grep -c steam)" -ne 0 ]
+    then
+        pkill steam
+        sleep 3
+    fi
+    wget -q -O output.dxvk-cache $url > /dev/null 2>&1
+    sleep .5
+    ./dxvk-cache-tool "$search_shader" "output.dxvk-cache"
+    rm "$search_shader"
+    mv output.dxvk-cache "$search_shader"
+else
+    echo "Script coud not locate r5apex.dxvk-cache"
 fi
 
-wget -q -O output.dxvk-cache $url > /dev/null 2>&1
-sleep .5
-if [  -f "$current_shader" ]
-then
-	./dxvk-cache-tool "$current_shader" "output.dxvk-cache"
-	rm "$current_shader"
-fi
-mv output.dxvk-cache "$current_shader"
+
